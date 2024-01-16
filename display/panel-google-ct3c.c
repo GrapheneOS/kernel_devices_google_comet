5eb1fc1e5d3ba83084bf779e53600933054374a2
derickhong <derickhong@google.com>
2024-01-16 11:48:20 +0800

    panel/ct3c: update brightness table
    Bug: 315055824
    Test: reboot to see brightness
    Change-Id: Iae507ad37916bb55ca793447138d669353d52659
    Signed-off-by: derickhong <derickhong@google.com>

--- display/panel-google-ct3c.c
+++ display/panel-google-ct3c.c
@@ -772,7 +772,7 @@ const struct brightness_capability ct3c_brightness_capability = {
 	.normal = {
 		.nits = {
 			.min = 2,
-			.max = 1250,
+			.max = 1200,
 		},
 		.level = {
 			.min = 184,
@@ -780,20 +780,20 @@ const struct brightness_capability ct3c_brightness_capability = {
 		},
 		.percentage = {
 			.min = 0,
-			.max = 78,
+			.max = 67,
 		},
 	},
 	.hbm = {
 		.nits = {
-			.min = 1250,
-			.max = 1600,
+			.min = 1200,
+			.max = 1800,
 		},
 		.level = {
 			.min = 3428,
-			.max = 3818,
+			.max = 4095,
 		},
 		.percentage = {
-			.min = 78,
+			.min = 67,
 			.max = 100,
 		},
 	},
@@ -853,7 +853,7 @@ const struct exynos_panel_desc google_ct3c = {
 	.data_lane_cnt = 4,
 	.max_brightness = 3818,
 	.min_brightness = 2,
-	.dft_brightness = 1268,    /* 140 nits */
+	.dft_brightness = 1290,    /* 140 nits */
 	.brt_capability = &ct3c_brightness_capability,
 	/* supported HDR format bitmask : 1(DOLBY_VISION), 2(HDR10), 3(HLG) */
 	.hdr_formats = BIT(2) | BIT(3),
