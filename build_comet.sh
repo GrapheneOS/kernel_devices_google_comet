3b293470111ffeaf471cb3b0292ae5e5ac8b5f00
Robin Peng <robinpeng@google.com>
2024-05-20 03:53:46 +0000

    build_<DEVICE>.sh: suppress messages while building with extra parameters
    Bug: 341316741
    Change-Id: I3eabb26af3604835d995d9a1f79c38129860d775
    Merged-In: Ib479b5833e59dfc4acc80b032282cdae0bf3af5e
    Signed-off-by: Robin Peng <robinpeng@google.com>

--- build_comet.sh
+++ build_comet.sh
@@ -13,23 +13,29 @@ if [ "${BUILD_AOSP_KERNEL}" = "1" -a "${BUILD_STAGING_KERNEL}" = "1" ]; then
   exit_if_error 1 "Flags incompatible with BUILD_AOSP_KERNEL detected"
 fi
 
-if [ "${#}" = "0" ]; then
-  parameters=
-  bazelrc_file="private/devices/google/common/device.bazelrc"
-  GKI_BUILD_ID=`sed -n 's/.*gki_prebuilts=\([0-9]\+\).*/\1/p' $bazelrc_file`
-  USE_GKI=`grep "^build --config=download_gki" $bazelrc_file`
-  NO_USE_GKI=`grep "^build --config=no_download_gki" $bazelrc_file`
-  USE_AOSP=`grep "^build --config=use_source_tree_aosp$" $bazelrc_file`
-  USE_AOSP_STAGING=`grep "^build --config=use_source_tree_aosp_staging$" $bazelrc_file`
-  if [ "${BUILD_AOSP_KERNEL}" = "1" ] || [ -n "${USE_AOSP}" ]; then
+parameters=
+bazelrc_file="private/devices/google/common/device.bazelrc"
+GKI_BUILD_ID=`sed -n 's/.*gki_prebuilts=\([0-9]\+\).*/\1/p' $bazelrc_file`
+USE_GKI=`grep "^build --config=download_gki" $bazelrc_file`
+NO_USE_GKI=`grep "^build --config=no_download_gki" $bazelrc_file`
+USE_AOSP=`grep "^build --config=use_source_tree_aosp$" $bazelrc_file`
+USE_AOSP_STAGING=`grep "^build --config=use_source_tree_aosp_staging$" $bazelrc_file`
+if [ "${BUILD_AOSP_KERNEL}" = "1" ] || [ -n "${USE_AOSP}" ]; then
+  if [ "${#}" = "0" ]; then
     echo -e "\nBuilding with core-kernel generated from source tree aosp/\n"
-    parameters="--config=use_source_tree_aosp --config=no_download_gki"
-  elif [ "${BUILD_STAGING_KERNEL}" = "1" ] || [ -n "${USE_AOSP_STAGING}" ]; then
+  fi
+  parameters="--config=use_source_tree_aosp --config=no_download_gki"
+elif [ "${BUILD_STAGING_KERNEL}" = "1" ] || [ -n "${USE_AOSP_STAGING}" ]; then
+  if [ "${#}" = "0" ]; then
     echo -e "\nBuilding with core-kernel generated from source tree aosp-staging/\n"
-    parameters="--config=use_source_tree_aosp_staging --config=no_download_gki"
-  elif [ -n "${GKI_BUILD_ID}" ] && [ -n "${USE_GKI}" ] && [ -z "${NO_USE_GKI}" ] && [ -z "${USE_AOSP}" ] && [ -z "${USE_AOSP_STAGING}" ]; then
+  fi
+  parameters="--config=use_source_tree_aosp_staging --config=no_download_gki"
+elif [ -n "${GKI_BUILD_ID}" ] && [ -n "${USE_GKI}" ] && [ -z "${NO_USE_GKI}" ] && [ -z "${USE_AOSP}" ] && [ -z "${USE_AOSP_STAGING}" ]; then
+  if [ "${#}" = "0" ]; then
     echo -e "\nBuilding with GKI prebuilts from ab/$GKI_BUILD_ID - kernel_aarch64\n"
-  else
+  fi
+else
+  if [ "${#}" = "0" ]; then
     echo -e "\nPlease check private/devices/google/common/device.bazelrc"
     echo -e "   1) IF \"build --config=use_source_tree_aosp\"           ----> core-kernel generated from source tree aosp/"
     echo -e "   2) IF \"build --config=use_source_tree_aosp_staging\"   ----> core-kernel generated from source tree aosp-staging/"
